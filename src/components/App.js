aeb6c9bdd7aade61f8f3cb73bb70c1e4493b6281
Tushar <gupta.tus@husky.neu.edu>
2019-03-28 17:31:22 -0400

    app converted to react

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 import React, {Component} from 'react';
+import '../styles.css';
 
 class App extends React.Component{
-    render() {
-        return <div>
-            <h1> My React App </h1>
-        </div>        
+
+    newTodo() {
+        alert('New ToDo button clicked and todo created!');
     }
+
+    render() {
+        return <div className="container center">
+          <h1 className="center title">My TODO App</h1>
+          <div className="flow-right controls">
+            <span>Item count: <span id="item-count">0</span></span>
+            <span>Unchecked count: <span id="unchecked-count">0</span></span>
+          </div>
+          <button className="button center" onClick={() => this.newTodo()}>New TODO</button>
+          <ul id="todo-list" className="todo-list"></ul>
+        </div>
+      }
 }
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,21 @@
-const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const htmlPlugin = new HtmlWebPackPlugin({template: "./src/index.html", filename: "./index.html"});
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: 'index_bundle.js'
-    },
-    
     module: {
         rules: [
             {
-                test: /\.js$/,   
-                exclude: /node_modules/,             
-                use:{
-                    loader:'babel-loader'
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
                 }
+            }, {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
             }
         ]
     },
-    plugins:[
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        })
-    ],
-    mode: 'development'
-}
-
+    plugins: [htmlPlugin]
+};
