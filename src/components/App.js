578098277fa2f5ca36022eccd53737034bf17d08
Tushar <gupta.tus@husky.neu.edu>
2019-03-28 18:08:52 -0400

    new todos are getting added

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,61 @@
 import React, {Component} from 'react';
 import '../styles.css';
 
+
+const classNames = {
+  TODO_ITEM: 'todo-container',
+  TODO_CHECKBOX: 'todo-checkbox',
+  TODO_TEXT: 'todo-text',
+  TODO_DELETE: 'todo-delete',
+}
+
+const uuidv1 = require('uuid/v1');
+
+
 class App extends React.Component{
+
+    constructor() {
+      super();
+
+      this.state = {
+        listofToDo: []
+      }
+    } 
 
     newTodo() {
         alert('New ToDo button clicked and todo created!');
+
+        var newTodo = {
+          id: uuidv1(),
+          title: 'My Todo'
+        }
+    
+        this.state.listofToDo.push(newTodo);
+    
+        this.setState({
+          listofToDo: this.state.listofToDo
+        });
+
     }
 
     render() {
+        const {listofToDo} = this.state;
         return <div className="container center">
-          <h1 className="center title">My TODO App</h1>
-          <div className="flow-right controls">
-            <span>Item count: <span id="item-count">0</span></span>
-            <span>Unchecked count: <span id="unchecked-count">0</span></span>
-          </div>
-          <button className="button center" onClick={() => this.newTodo()}>New TODO</button>
-          <ul id="todo-list" className="todo-list"></ul>
-        </div>
+                  <h1 className="center title">My TODO App</h1>
+                  <div className="flow-right controls">
+                    <span>Item count: <span id="item-count">0</span></span>
+                    <span>Unchecked count: <span id="unchecked-count">0</span></span>
+                  </div>
+                  <input type="text" ref="todoItem" placeholder="Enter Todo Item" required />
+                  <button className="button center" onClick={() => this.newTodo()}>New TODO</button>
+                  <ul id="todo-list" className="todo-list">
+                      {
+                      listofToDo.map(todo => {
+                        return <li className={classNames.TODO_ITEM} key={todo.id}><p className={classNames.TODO_TEXT}>{todo.title}</p></li>
+                      })
+                    }
+                  </ul>
+              </div>
       }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App';
 
+
+
 ReactDOM.render(<App />, document.getElementById('app'));
 
 
